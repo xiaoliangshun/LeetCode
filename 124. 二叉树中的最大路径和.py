4c595d65de30ce6c8c969577e4773d767a5fd1e8
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-01-12 00:20:38 +0800

    对于树的遍历+分治法+问题求解（拆分+重组）

---
+++ 124. 二叉树中的最大路径和.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:            ##后序的处理方法，从下层向上层？？？？？？想不出
+        def tailOrder(root):
+            if not root:
+                return -10**5           #负无穷
+            if root.val < 0:
+                return max(tailOrder(root.left),tailOrder(root.right))
+
+
+
+        #################      借鉴           #########33
+    def __init__(self):
+        self.maxSum = float("-inf")
+
+    def maxPathSum(self, root: TreeNode) -> int:
+        def maxGain(node):
+            if not node:
+                return 0
+
+            # 递归计算左右子节点的最大贡献值
+            # 只有在最大贡献值大于 0 时，才会选取对应子节点
+            leftGain = max(maxGain(node.left), 0)                                       ##左递归
+            rightGain = max(maxGain(node.right), 0)                                         ##右递归
+
+            # 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+            priceNewpath = node.val + leftGain + rightGain
+
+            # 更新答案        结果
+            self.maxSum = max(self.maxSum, priceNewpath)
+
+            # 返回节点的最大贡献值
+            return node.val + max(leftGain, rightGain)              ####如果要往上走，就不能同时取左右两个子树的最大值
+
+        maxGain(root)
+        return self.maxSum
