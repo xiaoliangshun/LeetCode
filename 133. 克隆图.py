9ffa5653b41ebd00f4cdbe00ba0e318ec8a84d38
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-03-21 16:43:57 +0800

    Add files via upload

---
+++ 133. 克隆图.py
@@ -0,0 +1,54 @@
+class Solution(object):
+
+    def __init__(self):
+        self.visited = {}
+
+    def cloneGraph(self, node):             ##深度优先遍历
+        if not node:
+            return node
+
+        # 如果该节点已经被访问过了，则直接从哈希表中取出对应的克隆节点返回，，，保证了后边的节点还能再指向前边已经访问的节点
+        if node in self.visited:
+            return self.visited[node]
+
+        # 克隆节点，注意到为了深拷贝我们不会克隆它的邻居的列表
+        clone_node = Node(node.val, [])
+
+        # 哈希表存储
+        self.visited[node] = clone_node
+
+        # 遍历该节点的邻居并更新克隆节点的邻居列表
+        if node.neighbors:
+            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]         ##递归他的左右邻居节点
+
+        return clone_node
+
+from collections import deque
+class Solution(object):
+
+    def cloneGraph(self, node):         ##广度优先遍历
+        if not node:
+            return node
+
+        visited = {}
+
+        # 将题目给定的节点添加到队列
+        queue = deque([node])
+        # 克隆第一个节点并存储到哈希表中
+        visited[node] = Node(node.val, [])
+
+        # 广度优先搜索
+        while queue:
+            # 取出队列的头节点
+            n = queue.popleft()
+            # 遍历该节点的邻居
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    # 如果没有被访问过，就克隆并存储在哈希表中
+                    visited[neighbor] = Node(neighbor.val, [])
+                    # 将邻居节点加入队列中
+                    queue.append(neighbor)
+                # 更新当前节点的邻居列表
+                visited[n].neighbors.append(visited[neighbor])
+
+        return visited[node]
