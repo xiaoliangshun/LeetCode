f282b81c4fd965064fe20ad349f6f3eb8951c0cf
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-06-20 18:29:23 +0800

    递归，或非递归

---
+++ 606. 根据二叉树创建字符串.java
@@ -0,0 +1,41 @@
+public class Solution {
+	//递归
+    public String tree2str(TreeNode t) {
+        if(t==null)
+            return "";
+        if(t.left==null && t.right==null)
+            return t.val+"";
+        if(t.right==null)
+            return t.val+"("+tree2str(t.left)+")";
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";   
+    }
+}
+
+public class Solution1 {
+	//非递归
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        Stack < TreeNode > stack = new Stack < > ();
+        stack.push(t);
+        Set < TreeNode > visited = new HashSet < > ();		//标记节点是否被访问过
+        StringBuilder s = new StringBuilder();
+        while (!stack.isEmpty()) {
+            t = stack.peek();		//获取栈顶元素
+            if (visited.contains(t)) {
+                stack.pop();
+                s.append(")");
+            } else {
+                visited.add(t);
+                s.append("(" + t.val);
+                if (t.left == null && t.right != null)		//单右支时
+                    s.append("()");
+                if (t.right != null)		//先右后左（保证了出栈时的左右顺序）
+                    stack.push(t.right);
+                if (t.left != null)
+                    stack.push(t.left);
+            }
+        }
+        return s.substring(1, s.length() - 1);
+    }
+}
