ed254df2accfa1cf3b6d8158a653aab34d79c64f
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-10-27 10:38:37 +0800

    原地数组计数

--- 636. 函数的独占时间.java
+++ 636. 函数的独占时间.java
@@ -1,23 +1,16 @@
-public class Solution {
-    public int[] exclusiveTime(int n, List < String > logs) {
-        Stack < Integer > stack = new Stack < > ();     //栈
-        int[] res = new int[n];
-        String[] s = logs.get(0).split(":");
-        stack.push(Integer.parseInt(s[0]));
-        int i = 1, prev = Integer.parseInt(s[2]);
-        while (i < logs.size()) {
-            s = logs.get(i).split(":");
-            if (s[1].equals("start")) {     //是start，一定要入栈，记录前一个的运行时间
-                if (!stack.isEmpty())
-                    res[stack.peek()] += Integer.parseInt(s[2]) - prev;
-                stack.push(Integer.parseInt(s[0]));
-                prev = Integer.parseInt(s[2]);
-            } else {            //是end，一定会跟前边的匹配，记录这一对的运行时间
-                res[stack.peek()] += Integer.parseInt(s[2]) - prev + 1;
-                stack.pop();
-                prev = Integer.parseInt(s[2]) + 1;
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            nums[(nums[i]-1)%(n+1)] += (n+1);       //一位偏移，和n+1
+        }
+        int[] res = new int[2];
+        for (int i = 0; i < n; i++) {
+            if(nums[i]/(n+1)==2){
+                res[0] = i+1;
+            }else if(nums[i]/(n+1)==0){
+                res[1] = i+1;
             }
-            i++;
         }
         return res;
     }
