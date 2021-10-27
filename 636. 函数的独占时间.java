1cc77bbe3bc87e35e2af09051a6c7e1d9efde0c3
XiaoLS-xls <74584911+XiaoLS-xls@users.noreply.github.com>
2021-10-27 10:11:54 +0800

    栈处理匹配

---
+++ 636. 函数的独占时间.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] exclusiveTime(int n, List < String > logs) {
+        Stack < Integer > stack = new Stack < > ();     //栈
+        int[] res = new int[n];
+        String[] s = logs.get(0).split(":");
+        stack.push(Integer.parseInt(s[0]));
+        int i = 1, prev = Integer.parseInt(s[2]);
+        while (i < logs.size()) {
+            s = logs.get(i).split(":");
+            if (s[1].equals("start")) {     //是start，一定要入栈，记录前一个的运行时间
+                if (!stack.isEmpty())
+                    res[stack.peek()] += Integer.parseInt(s[2]) - prev;
+                stack.push(Integer.parseInt(s[0]));
+                prev = Integer.parseInt(s[2]);
+            } else {            //是end，一定会跟前边的匹配，记录这一对的运行时间
+                res[stack.peek()] += Integer.parseInt(s[2]) - prev + 1;
+                stack.pop();
+                prev = Integer.parseInt(s[2]) + 1;
+            }
+            i++;
+        }
+        return res;
+    }
+}
